eadd504cb8d4745dcb5d767cb6ec5ecad9c0bb69
Glenn Cottam <glenn.cottam@hotmail.com>
2020-03-22 13:54:38 -0300

    Series, Datatables, and Config file
    - Series: Monym now pulls data from Sonarr with data regarding series. Monym now displays said data with a searchable table.
    - Datatables: Searching and filtering
    - Config File: allows easy easier configuration for sonarr, radarr ports, URL's and API keys.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const fs = require('fs');
 const port = 6969;
 
 const config = JSON.parse(fs.readFileSync('config/config.json'));
-console.log("Config File loaded: \n" + JSON.stringify(config));
+console.log("Config File loaded");
 
 app.use(cors());
 app.set('view engine', 'ejs');
@@ -19,10 +19,17 @@ app.use(Express.static('public'));
 // WebServer
 app.get('/', async function(req, res)
 {
+    console.log("Grabbing Info From Sonarr");
     var info = await sonarr("/system/status");
+
+    console.log("Grabbing Download Queue From Sonarr");
     var queue = await sonarr("/queue");
+    
+    console.log("Grabbing Series From Sonarr");
+    var series = await sonarr("/series");
+
     var message = "hello world";
-    res.render('index', {message: message, info: info, queue: queue});
+    res.render('index', {message: message, info: info, queue: queue, series});
     console.log("GET Request for '/'");
 });
 
